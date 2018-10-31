757c9ba50bb757b66b3536e1029295e5ff0f57ef
AeroCatDavid <44610254+AeroCatDavid@users.noreply.github.com>
2018-10-31 20:55:08 +0100

    Create HttpGet.cpp

---
+++ HttpGet.cpp
@@ -0,0 +1,22 @@
+Hello chief. I guess you found my github. If your name is 'superskater' then please leave lmao.
+
+This Httpget is for c++ and it works on Axon. It is very simple and i take all credits.
+I havent tested it yet but i trust myself. You need DownloadURL thing for c++ or else it wont be able to download the script!
+
+
+if(Script == std::string("HttpGet")) {
+        std::string shit;
+        std::string httpscript;
+        shit = "loadstring(game:HttpGet(('";
+        shit += httpscript;
+        shit += "'),true))()";
+
+         std::string FinalScriptHttpGet = DownloadURL(httpscript);
+         ExecuteScript(FinalScriptHttpGet);
+    }
+    
+    
+    I had some issues on this because i don't know if the *if(Script == std::string("HttpGet")) {* part will work. 
+    But you try it fam. All for you <3
+    
+    I bet all Axon exploits will have HttpGet now lol.
